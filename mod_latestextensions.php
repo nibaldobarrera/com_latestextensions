0e8f1fd41f8acbf6507d70adfc9806a890784b40
Nibaldo Barrera <nibaldobarrera@kazeta.cl>
2016-11-16 05:41:36 -0300

    V1.1.0 Se hace compatuble con Joomla 2.5.5

--- mod_latestextensions.php
+++ mod_latestextensions.php
@@ -4,3 +4,9 @@ require_once __DIR__ . '/helper.php';
 $list = mod_latestextensionsHelper::getList($params);
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 require JModuleHelper::getLayoutPath('mod_latestextensions',$params->get('layout', 'default'));
+$version = new JVersion();
+if(floatval($version->RELEASE) <= '2.5')
+{
+JHtml::_('stylesheet', 'mod_latestextensions/style.css',
+array(), true);
+}
